542ca488aa2900fe647313064994c51d09810ea0
Lemsic <louislemsic@gmail.com>
2021-04-20 03:37:42 +0800

    Alpha Update 1.2

--- global_src/main_scripts.js
+++ global_src/main_scripts.js
@@ -1,1 +1,18 @@
 //all the scripts go here
+
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains_behind = document.getElementById('mountains_behind');
+let text = document.getElementById('text');
+let btn = document.getElementById('main_btn');
+let mountains_front = document.getElementById('mountains_front');
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    stars.style.left = value * 0.75 + 'px';
+    moon.style.top = value * 1.05 + 'px';
+    mountains_behind.style.top = value * 0.5 + 'px';
+    mountains_front.style.top = value * 0 + 'px';
+    header.style.top = value * 0.5 + 'px';
+})
